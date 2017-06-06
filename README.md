b1ddc7f4c012a5171086bee7670b928516d7e198
chang_su <chang_su@hunliji.com>
2017-06-06 17:51:52 +0800

    html-webpack-plugin

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # webpack-quick-start
 A quick-start demo for webpack.
+
+## How to use
+`npm install && npm start`
+
+## Description
+- To keep things convenient to maintain, you can use your first plugin: `html-webpack-plugin`. HtmlWebpackPlugin generates an index.html for the application and adds a script tag to load the generated bundle. Install it:
+
+`npm install html-webpack-plugin --save-dev`
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,46 @@
 var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './js/entry.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "> 1%"]}',
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  },
+    entry: './js/entry.js',
+    output: {
+        path: __dirname,
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version", "> 1%"]}',
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
 
-  plugins: [
-      //生成环境使用，可减小压缩体积
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    // new webpack.LoaderOptionsPlugin({
-    //   options: {
-    //     postcss: function () {
-    //       return [autoprefixer];
-    //     },
-    //   }
-    // })
-  ]
+    plugins: [
+        //生成环境使用，可减小压缩体积
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './app/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        // new webpack.LoaderOptionsPlugin({
+        //   options: {
+        //     postcss: function () {
+        //       return [autoprefixer];
+        //     },
+        //   }
+        // })
+    ]
 }
